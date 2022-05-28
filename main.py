db8e9d0ef84dc5a6864547b5f91d68f6e4526a5b
maxxfreund <78813326+maxxfreund@users.noreply.github.com>
2022-05-28 18:58:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ async def on_ready():
 #bot's content
   testing_channel = client.get_channel(980145563326644255)
 
-  await testing_channel.send("what up dawg")
+  await testing_channel.send("You turned me on ;P")
 #Run client on server
 client.run('OTgwMTQ2NTEyODc5MzE3MDIy.GZfrmU.iI40W1SagHrEiO0T-aijZT_rF3Mgcn35vL0ZW0')
 
