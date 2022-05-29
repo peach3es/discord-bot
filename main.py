09a5ba405ccb03fb786327c01b4a09350fbc5e0b
peaches <96403683+peach3es@users.noreply.github.com>
2022-05-29 02:40:39 -0400

    init

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import discord 
-#from discord.ui import button
 from discord.ext  import commands
-# client = discord.Client() #client
 
 TOKEN = "OTgwMTQ2NTEyODc5MzE3MDIy.GZfrmU.iI40W1SagHrEiO0T-aijZT_rF3Mgcn35vL0ZW0"
 
-bot = commands.Bot(command_prefix = '_')
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix = "#", intents = intents)
 
 @bot.event
 async def on_ready():
-  testing_channel = bot.get_channel(980145563326644255)
-  await testing_channel.send("I'm turned on ;P")
+  print("The bot is ready")
 
-@bot.command(pass_context = True)
-async def play(ctx):
-#bot's content
+@bot.event
+async def on_message(msg):
+  if msg.author.id == bot.user.id:
+    return
+  await bot.process_commands(msg)
+
+@bot.command()
+async def game(ctx):
   myembed = discord.Embed(
     title="Game List", 
     colour=discord.Colour(0xbc708f),
@@ -22,10 +26,11 @@ async def play(ctx):
 
   myembed.set_author(name="#play", url="https://discordapp.com")
   myembed.set_footer(text="#play")
-  myembed.add_field(name="Tic Tac Toe", value="A classic game of tic tac toe to play with your friends (1 vs 1) ```#play tictactoe @mention```")
+  myembed.add_field(name="Tic Tac Toe", value="A classic game of tic tac toe to play with your "
+                                              "friends (1 vs 1) ```#play tictactoe @mention```")
+  
+  await ctx.reply(embed = myembed)
 
-  await ctx.send(embed=myembed)
-#Run client on server
 bot.run(TOKEN)
 
 
